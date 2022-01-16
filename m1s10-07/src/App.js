f3d0396bbc4f3de4a8cf4ab2112eba4deb8a5563
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2022-01-16 18:20:37 -0300

    removendo arquivos desnecessarios

--- m1s10-07/src/App.js
+++ m1s10-07/src/App.js
@@ -3,20 +3,17 @@ import './App.css';
 import { AuthConsumer } from './components/consumer/authConsumer';
 
 import { AuthContext } from './contexts/auth';
-import { ThemeContext } from './contexts/theme';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
   const [theme, setTheme] = useState('ligth');
   return (        
-    <div className="App">
-      <ThemeContext.Provider value={{theme, setTheme}}>
-        <header className=  "App-header">                
-          <AuthContext.Provider value={{authenticated, setAuthenticated}}>
-            <AuthConsumer />
-          </AuthContext.Provider>        
-        </header>
-      </ThemeContext.Provider>
+    <div className="App">      
+      <header className=  "App-header">                
+        <AuthContext.Provider value={{authenticated, setAuthenticated}}>
+          <AuthConsumer />
+        </AuthContext.Provider>        
+      </header>      
     </div>
   );
 }
